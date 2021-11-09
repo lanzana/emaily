71d35e737cc7adbf238e82e0369fbb3813cf4fe0
Gustavo Lanzana <67080686+lanzana@users.noreply.github.com>
2021-11-08 21:50:11 -0300

    updated dev and prod configs

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,5 @@
-module.exports = {
-  googleClientID:
-    "968549470888-j1lerrntqaaahke16v8fe4csa7cl4ltu.apps.googleusercontent.com",
-  googleClientSecret: "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",
-  mongoURI:
-    "mongodb+srv://admin:H7vjsS7PoV22D8r9@emaily.onjuh.mongodb.net/emailyDB?retryWrites=true&w=majority",
-  cookieKey: "shisaudhwindjkncaoniondpq0930912keo3#!@321asdowpdqow",
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./prod");
+} else {
+  module.exports = require("./dev");
+}
